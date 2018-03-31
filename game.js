c2c9e30040ebe8a276a5bf7753fc5bca43eae7d7
Enya Kuo <kuoenya@gmail.com>
2018-03-31 21:47:34 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,2 +1,33 @@
 "use strict";
         console.log('okok');
+var cell= document.querySelector('.cell[data-i="1"]');
+cell.onclick= function(){
+    console.log("okkkkk:) ");
+};
+cell.onmouseenter= function(){
+    this.innerHTML= "X ";
+};
+cell.onmouseleave= function(){
+    this.innerHTML= "";
+};
+//jQuery
+//$('.cell[data-i="2"]')
+   // click(function(){
+   //     console.log("okkkkk:) ");
+   // })
+   // .mouseenter(function(){
+   //     $(this).HTML= "O";
+   // })
+   // .mouseleave(function(){
+   //     $(this).HTML="";
+   // })
+   var cell= document.querySelector('.cell[data-i="2"]');
+cell.onclick= function(){
+    console.log("okkkkk:) ");
+};
+cell.onmouseenter= function(){
+    this.innerHTML= "O ";
+};
+cell.onmouseleave= function(){
+    this.innerHTML= "";
+};
